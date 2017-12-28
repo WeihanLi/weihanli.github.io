827129bec4e65feead8d17a6691bce3810a5ec08
liweihan <weihanli@outlook.com>
2017-12-28 23:51:57 +0800

    代码优化

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ function getRouteData(){
     if(array.length>1){
         var modules = ['about','education','projects','skills'];
         for(var i=0;i<modules.length;i++){
-            if(modules[i] == array[1]){
+            if(modules[i] === array[1]){
                 return array[1];
             }
         }
@@ -22,6 +22,16 @@ angular.module("cv",[]).controller("CvCtrl",function($scope,$http){
         $scope.isSelected = function(item){
             return item == $scope.item;
         };
+
+        $scope.socialLinks = [
+            { "title":"outlook","iconPath":"./images/social_icons/outlook.ico","linkUrl":"mailto:weihanli@outlook.com" },
+            { "title":"github","iconPath":"./images/social_icons/github.ico","linkUrl":"https://github.com/WeihanLi" },
+            { "title":"osChina","iconPath":"./images/social_icons/oschina.ico","linkUrl":"https://gitee.com/weihanli" },
+            { "title":"gitbook","iconPath":"./images/social_icons/gitbook.ico","linkUrl":"https://www.gitbook.com/@weihanli" },
+            { "title":"cnblogs","iconPath":"./images/social_icons/cnblogs.ico","linkUrl":"https://weihanli.cnblogs.com/" },
+            { "title":"jianshu","iconPath":"./images/social_icons/jianshu.ico","linkUrl":"https://www.jianshu.com/u/9ea357af743b" },
+            { "title":"weibo","iconPath":"./images/social_icons/weibo.ico","linkUrl":"https://weibo.com/weihanli" }            
+        ];
 
         // activities
         $scope.activities = null;
